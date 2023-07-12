f6f46b28df0cf02be64c967fb114b2c5c58d76c4
liubavabarsegian <lyubahemmo@mail.ru>
2023-07-12 22:54:12 +0300

    configured scale to normal (static yet), drawing verticles and edges

--- src/3d-viewer/opengl.cpp
+++ src/3d-viewer/opengl.cpp
@@ -17,23 +17,21 @@ void OpenGL::initializeGL()
 {
     model.count_of_vertices = 15;
     model.count_of_facets = 15;
-    qDebug("kek %d", model.count_of_vertices);
+    qDebug("V %d", model.count_of_vertices);
     model.vertices = (vertice *)malloc(model.count_of_vertices * sizeof(vertice));
     if (model.vertices != NULL) {
       model.polygons = (polygon_t *)malloc(model.count_of_facets * sizeof(polygon_t));
       if (model.polygons != NULL) {
         open_and_parse(&model);
+        qDebug("V %d", model.count_of_vertices);
       }
     }
-    qDebug("kek %d", model.count_of_vertices);
 
     //загрузка единичной матрицы
 //   glLoadIdentity();
    //преобразование матрицы так, чтобы
    //создавалась ортогональная проекци
    glOrtho(0, 800, 0, 600, 1, 100);
-   //для отдаления и приближения изображения
-//    glFrustum(-1, 1, -1, 1, 1, 3);
 }
 
 void OpenGL::paintGL()
@@ -45,7 +43,9 @@ void OpenGL::paintGL()
     glLoadIdentity();
     glRotatef(xRot, 1, 0, 0);
     glRotatef(yRot, 0, 1, 0);
-    drawModel(0.5);
+    glScalef(0.001, 0.001, 0.001);
+    glFlush();
+    drawModel(100);
 }
 
 void OpenGL::resizeGL(int w, int h)
@@ -56,6 +56,14 @@ void OpenGL::resizeGL(int w, int h)
 
 void OpenGL::drawModel(float a)
 {
+    float verticles[] = {
+            -a, -a, a,   a, -a, a,     a, a, a,     -a, a, a,
+            a, -a, -a,   -a, -a, -a,   -a, a, -a,   a, a, -a,
+            -a, -a, -a,   -a, -a, a,   -a, a, a,    -a, a, -a,
+            a, -a, a,    a, -a, -a,    a, a, -a,    a, a, a,
+            -a, -a, a,    a, -a, a,    a, -a, -a,    -a, -a, -a,
+            -a, a, a,    a, a, a,     a, a, -a,     -a, a, -a
+        };
 //    //glVertexPointer(количество координат, тип, смещение, адрес)
 //    glVertexPointer(3, GL_FLOAT, 0, &arr_cube);
 //    //разрешение использования вершинного буфера
@@ -90,17 +98,43 @@ void OpenGL::drawModel(float a)
          glVertex3f(0.0, 0.0, 1.0);
     glEnd();
 
+    //рисует вершины
     glBegin(GL_POINTS);
-
-        for (size_t i = 0; i <  model.count_of_vertices; i++)
+        for (size_t i = 0; i <  24; i++)
         {
-            float x = model.vertices[i].x;
-            float y = model.vertices[i].y;
-            float z = model.vertices[i].z;
-            glVertex3f(x, y, z);
-            qDebug("x: %f y: %f z: %f");
+            float x = verticles[i];
+            float y = verticles[i + 1];
+            float z = verticles[i + 2];
+            i+= 2;
+            glVertex3d(x, y, z);
+            qDebug("x: %f y: %f z: %f", x, y, z);
         }
     glEnd();
+
+    //соединяет линии по точкам подряд
+    glBegin(GL_LINE_STRIP);
+        for (size_t i = 0; i <  24; i++)
+        {
+            float x = verticles[i];
+            float y = verticles[i + 1];
+            float z = verticles[i + 2];
+            i+= 2;
+            glVertex3f(x, y, z);
+            qDebug("x: %f y: %f z: %f", x, y, z);
+        }
+    glEnd();
+
+//    glBegin(GL_POINTS);
+
+//        for (size_t i = 0; i <  model.count_of_vertices; i++)
+//        {
+//            float x = model.vertices[i].x;
+//            float y = model.vertices[i].y;
+//            float z = model.vertices[i].z;
+//            glVertex3f(x, y, z);
+//            qDebug("x: %f y: %f z: %f", x, y, z);
+//        }
+//    glEnd();
 }
 
 void OpenGL::mousePressEvent(QMouseEvent *mo)
